8d2c832dc8694334b4c45bf8d36c39b1d82b6b95
FAC <foxtrotalfa.fac@gmail.com>
2023-01-31 00:34:50 -0300

    Agregado de Form. Contacto

--- js/obtenerPersona.js
+++ js/obtenerPersona.js
@@ -1,22 +1,19 @@
 function getUsers() {
-    fetch("https://randomuser.me/api/")
-      .then((results) => {
-        return results.json();
-      })
-      .then((data) => {
-        console.log(data.results['0']);
-        // Access your data here
-        //  const persona = JSON.parse(data.results[0]);
-        // document.getElementById("nombre").innerHTML = persona.name.first+", "+ persona.name.last;
-        document.getElementById("nombre").innerHTML =  data.results['0'].name.first + " " + data.results['0'].name.last;
-        document.getElementById("email").innerHTML = " " + data.results['0'].email;
-        document.getElementById("tel").innerHTML = " " + data.results['0'].cell;
-        document.getElementById("ldin").innerHTML = " /" + data.results['0'].login.username;
-        document.getElementById("git").innerHTML = " @" + data.results['0'].login.username;
-        document.getElementById("fotoPerfil").setAttribute("src", data.results['0'].picture.medium);
-        document.getElementById("domicilio").innerHTML = " " + data.results['0'].location.street.name + " " + data.results['0'].location.street.number + ", " + data.results['0'].location.city;
+  fetch("https://randomuser.me/api/")
+    .then((results) => {
+      return results.json();
+    })
+    .then((data) => {
+      // console.log(data.results['0']);
+      document.getElementById("nombre").innerHTML = data.results['0'].name.first + " " + data.results['0'].name.last;
+      document.getElementById("email").innerHTML = " " + data.results['0'].email;
+      document.getElementById("tel").innerHTML = " " + data.results['0'].cell;
+      document.getElementById("ldin").innerHTML = " /" + data.results['0'].login.username;
+      document.getElementById("git").innerHTML = " @" + data.results['0'].login.username;
+      document.getElementById("fotoPerfil").setAttribute("src", data.results['0'].picture.medium);
+      document.getElementById("domicilio").innerHTML = " " + data.results['0'].location.street.name + " " + data.results['0'].location.street.number + ", " + data.results['0'].location.city;
 
-      });
-  }
+    });
+}
 
-  getUsers();
+getUsers();
